c1a6f55350e555f9f53703846b37215466b633b9
stevekalix <msr962005@gmail.com>
2026-08-12 12:13:32 +0530

    LongestCommonPrifix

---
+++ SubPre.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class SubPre {
+
+    public static void main(string s[]) {
+        String[] arr = { "dog", "racecar", "car" };
+
+        Arrays.sort(arr);
+
+        for (String s : arr) {
+            System.out.println(s);
+        }
+    }
+}
